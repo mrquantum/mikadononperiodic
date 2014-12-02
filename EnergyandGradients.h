229520acf882c9b4856545e2489cbf2abbce02af
Mathijs <Mathijs Vermeulen>
2014-12-02 17:22:39 +0100

    Forgot rhe rest of the files

--- EnergyandGradients.h
+++ EnergyandGradients.h
@@ -46,63 +46,11 @@ double dEda(const Eigen::VectorXd &XY,
             const Eigen::VectorXd &s0,
             const std::vector<spring> &springlist,
             const std::vector<std::vector<int>> &springpairs,
-            double kappa);//,const std::vector<triplet> &tripl,const int BendOn); 
+            double kappa); //,const std::vector<triplet> &tripl,const int BendOn); 
 
 double quad(double x);
 
-int doBracketfind(double &a1,double &a2,
-                   const Eigen::VectorXd &XY,
-                   const Eigen::VectorXd &s0,
-                   const std::vector<spring> &springlist,
-                   const std::vector<std::vector<int>> &springpairs, 
-                   double kappa);
 
-void doBisection(double a1,double a2,double &root,
-                 const Eigen::VectorXd &XY,
-                 const Eigen::VectorXd &s0,
-                 const std::vector<spring> &springlist,
-                 const std::vector<std::vector<int>> &springpairs, 
-                 double kappa);
-
-
-void doFalsePosition(double &xl,double &xh,double &root,
-                 const Eigen::VectorXd &XY,
-                 const Eigen::VectorXd &s0,
-                 const std::vector<spring> &springlist,
-                 const std::vector<std::vector<int>> &springpairs, 
-                 double kappa);
-
-
-void doSecant(   double &root,
-                 const Eigen::VectorXd &XY,
-                 const Eigen::VectorXd &s0,
-                 const std::vector<spring> &springlist,
-                 const std::vector<std::vector<int>> &springpairs, 
-                 double kappa);
-
-
-void doConjStep(Eigen::VectorXd &XY,
-                Eigen::VectorXd &s0,
-                Eigen::VectorXd &gradE,
-                std::vector<spring> &springlist,
-                std::vector<std::vector<int>> &springpairs,
-                double &root,
-                double kappa,
-                int conjstep);
-
-void doSteepestDescent(Eigen::VectorXd &XY,
-                Eigen::VectorXd &s0,
-                Eigen::VectorXd &gradE,
-                std::vector<spring> &springlist,
-                std::vector<std::vector<int>> &springpairs,
-                double &root,
-                double kappa);
-                //Eigen::VectorXd &b);
-
-Eigen::VectorXd Hessianapprox(const Eigen::VectorXd &XY,
-                              const Eigen::VectorXd &XYm1,
-                              const Eigen::VectorXd &g0,
-                              const Eigen::VectorXd &g0m1);
 
 
 
--- main.cpp
+++ main.cpp
@@ -3,6 +3,7 @@
 #include "random.h"
 #include "makemikadonetwork.h"
 #include "EnergyandGradients.h"
+#include "minimizers.h"
 #include <eigen3/Eigen/Core>
 #include <eigen3/Eigen/LU>
 #include <fstream>
@@ -11,6 +12,7 @@
 #include <vector>
 #include <math.h>
 #include <functional>
+#include "importparam.h"
 
 using namespace std;
 using namespace Eigen;
@@ -25,6 +27,65 @@ void getRandom(vector<double> &v)
   }
 }
   
+int get_next_script_tag(FILE *in, char *buf)
+{
+        if(fscanf(in, "%*[^<]") != 0) {
+                printf("White space removal error in get_next_script_tag(). Something odd has happened: this error should never occur...\n");
+        }
+        if(fscanf(in, "<%255[^>]>", buf) != 1) {
+                printf("Error reading tag in script file.\n");
+                return 0;
+        }
+
+        return 1;
+} 
+ 
+   
+int init(param &Mikadoparameters ,const char *FILENAME)
+{
+    FILE *paramfile;
+    paramfile=fopen(FILENAME,"r");
+    if(!paramfile){
+     cout<<"Error opening paramfile"<<endl;
+     return -1;     
+    }
+    const int buffersize=101;
+    char buf[buffersize];
+//     if(get_next_script_tag(paramfile,buf)==0){
+//         cout<<"Error reading script file"<<endl;   
+//         fclose(paramfile);
+//         return -1;
+//    }
+
+for(;;) {
+        if(get_next_script_tag(paramfile, buf) == 0) {
+                printf("\tError when reading tag in scriptfile\n");
+                fclose(paramfile);
+                return -1;
+        }
+//         if(feof(script_file)) {
+//                 FFEA_error_text();
+//                 printf("\tReached end of file before end of <param> block\n");
+//                 fclose(script_file);
+//                 return FFEA_ERROR;
+//         }
+
+        if(strcmp(buf, "END") == 0) {
+                break;
+        }
+
+        if(Mikadoparameters.parse_param_assignment(buf) == -1) {
+                printf("Parameter assignment failed.\n");
+                fclose(paramfile);
+                return -1;
+        }
+}    
+ return 0;   
+}
+ 
+ 
+ 
+ 
 int main (int argc,char **argv)
 {
   
@@ -34,158 +95,185 @@ if(argc>1){
 }  
 //my_random::set_seed(0);
 //System parameters  
-int Nit=500;  
-double tolE=.001;
-int NumberMikado=100;
-double LStick=.4; //Stick Length
-double k1=.05;
-double k2=.1;
-double kappa=.000007;
-double rlenshort=.0001;
-double rlenlong=.01;
 
+
+
+param Mikadoparameters;
+//char parameterfile = "params.txt";
+
+init(Mikadoparameters,"params.txt");
+
+double ESTRETCH,EBEND,ETOT;
+vector<int> order;
 vector<stick> mikado;
 vector<stick> mikorig;
-makeSticks(mikado,mikorig,NumberMikado,LStick);
 vector<connected> Connection;
-makeConnections(Connection,mikado,LStick);
-//Here we create the nodes, and the springs from the conncection structure that has already
-//been made above. 
 vector<elonstick> ELONSTICK;
-sortELEMENTSperMIKADO(ELONSTICK,Connection);
-vector<int> order;
-orderElonstick(order,ELONSTICK); 
-
-//Make the springs and Nodes. Input springlist and nodes are (empty vectors)
 vector<spring> springlist(0);
 vector<node> nodes(0);
-makeSpringsAndNodes(ELONSTICK,mikorig,springlist,nodes,rlenshort,rlenlong,k1,k2); 
-
-//Remove all double info
 vector<node> singleNodes; 
+vector<vector<int>> springpairs(0);
+double lenGrad;
+vector<double> alpha(100);
+
+int Nit=Mikadoparameters.Nit;  
+double tolE=Mikadoparameters.tolE;
+int NumberMikado=Mikadoparameters.NumberMikado;
+double LStick=Mikadoparameters.LStick; //Stick Length
+double k1=Mikadoparameters.k1;
+double k2=Mikadoparameters.k2;
+double kappa=Mikadoparameters.kappa;
+double rlenshort=Mikadoparameters.rlenshort;
+double rlenlong=Mikadoparameters.rlenlong;
+
+
+
+ofstream mikadofile("mikado.txt"); 
+ofstream nodefile("nodes.txt");
+ofstream springfile("springs.txt");
+ofstream XYfile("conjpoints.txt");
+ofstream EFile("Energy.txt");
+ofstream dEdafile("dEda.txt");
+ofstream Rootalpha("rootalpha.txt");
+
+
+
+
+makeSticks(mikado,mikorig,NumberMikado,LStick);
+
+    //write sticks to mikado.txt
+for(int i=0;i<mikado.size();i++){
+    mikadofile<<mikado[i].nr<<"\t"<<mikado[i].x<<"\t"<<mikado[i].y<<"\t"<<mikado[i].th<<"\t"<<mikado[i].wlr<<
+    mikado[i].wud<<endl;
+} mikadofile.close();
+
+makeConnections(Connection,mikado,LStick);
+     //Here we create the nodes, and the springs from the conncection structure that has already
+     //been made above. 
+sortELEMENTSperMIKADO(ELONSTICK,Connection);
+orderElonstick(order,ELONSTICK); 
+ 
+     //Make the springs and Nodes. Input springlist and nodes are (empty vectors)
+makeSpringsAndNodes(ELONSTICK,mikorig,springlist,nodes,rlenshort,rlenlong,k1,k2); 
+    
+//write sticks to springs.txt
+for(int i=0;i<springlist.size();i++){
+    cout<<"(*&(&*(&  "<<springlist[i].rlen<<endl;
+    springfile<<springlist[i].one<<"\t"
+              <<springlist[i].two<<"\t"
+              <<springlist[i].wlr<<"\t"
+              <<springlist[i].wud<<"\t"
+              <<springlist[i].rlen<<"\t"
+              <<springlist[i].k<<"\t"
+              <<springlist[i].sticki<<endl;
+}       springfile.close();
+
+
+
+
+     //Remove all double info
 for(std::size_t i=0;i<nodes.size();i++){
-  if(nodes[i].number!=nodes[i+1].number){
-    node unique=nodes[i];   
-    singleNodes.push_back(unique);  
-  }
+   if(nodes[i].number!=nodes[i+1].number){
+     node unique=nodes[i];   
+     singleNodes.push_back(unique);  
+   }
+}
+     //**************MAKE HERE THE PAIR OF SPRINGS
+makeSpringpairs(springpairs,springlist);
+for(int i=0;i<singleNodes.size();i++){
 }
 
-//**************MAKE HERE THE PAIR OF SPRINGS
-vector<vector<int>> springpairs(0);
-makeSpringpairs(springpairs,springlist);
+VectorXd X(singleNodes.size()),Y(singleNodes.size());
+VectorXd XY(2*X.size());
+VectorXd gradE(XY.size());
+VectorXd XYn(XY.size());
+VectorXd XYcopy(XY.size());
+VectorXd gradEn(gradE.size());
+VectorXd s0(gradE.size());
 
 
-//The xy positions
- VectorXd X(singleNodes.size()),Y(singleNodes.size());
- for(std::size_t i=0;i<singleNodes.size();i++){
-    X(i)=singleNodes[i].x; 
-    Y(i)=singleNodes[i].y;
- }
- for(std::size_t i=0;i<singleNodes.size();i++){
-    X(i)=inbox(X(i),1.0);
-    Y(i)=inbox(Y(i),1.0);
- }
-  
- VectorXd XY(2*X.size());
+for(int i=0;i<singleNodes.size();i++){
+    nodefile<<singleNodes[i].number<<"\t"<<singleNodes[i].x<<"\t"<<singleNodes[i].y<<endl;
+} nodefile.close();
+
+
+     //The xy positions
+  for(std::size_t i=0;i<singleNodes.size();i++){
+     X(i)=singleNodes[i].x; 
+     Y(i)=singleNodes[i].y;
+  }
+  for(std::size_t i=0;i<singleNodes.size();i++){
+     X(i)=inbox(X(i),1.0);
+     Y(i)=inbox(Y(i),1.0);
+  }
+   
  XY<<X,Y;
  
  
-double ESTRETCH=Energynetwork(springlist,XY);
-double EBEND=Ebend(springpairs,springlist,XY,kappa);
-double ETOT=ESTRETCH+EBEND;
+ESTRETCH=Energynetwork(springlist,XY);
+EBEND=Ebend(springpairs,springlist,XY,kappa);
+ETOT=ESTRETCH+EBEND;
 
-//Here comes the conjugate gradient
-    VectorXd gradE(XY.size());
-    VectorXd XYn(XY.size());
-    VectorXd XYcopy(XY.size());
-    VectorXd gradEn(gradE.size());
-    VectorXd s0(gradE.size());
-    double betan;
-    double Estr, Eben, Etot, lenGrad;
+    //Here comes the conjugate gradient
 
-    gradE=Gradient(springlist,XY)+gradEbend(springpairs,springlist,XY,kappa);
-    s0=-gradE; 
+gradE=Gradient(springlist,XY)+gradEbend(springpairs,springlist,XY,kappa);
+s0=-gradE; 
      
-    ofstream XYfile("conjpoints.txt");
-    ofstream EFile("Energy.txt");
-    ofstream dEdafile("dEda.txt");
-    ofstream Rootalpha("rootalpha.txt");
 
-    vector<double> alpha(200);
-    for(int i=0;i<200;i++){
-        alpha[i]=-.1+0.001*i;
-        dEdafile<<alpha[i]<<"\t";    
-    }
-    dEdafile<<endl;
+
+//     for(int i=0;i<99;i++){
+//         alpha[i]=-1+.02*i;
+//         dEdafile<<alpha[i]<<"\t";    
+//     }
+//     dEdafile<<endl;
     
-    EFile<<ESTRETCH<<"\t"<<EBEND<<"\t"<<ETOT<<"\t"<<0<<endl;
+EFile<<ESTRETCH<<"\t"<<EBEND<<"\t"<<ETOT<<"\t"<<0<<endl;
  
     //The loop of the conj grad method
-    int conjsteps=0;
-    double root1;
-    double ETOT0;
-    ESTRETCH=Energynetwork(springlist,XY);
-    EBEND=Ebend(springpairs,springlist,XY,kappa);
-    ETOT0=ESTRETCH+EBEND;
-    
-    
+int conjsteps=0;
+double root1;
+ESTRETCH=Energynetwork(springlist,XY);
+EBEND=Ebend(springpairs,springlist,XY,kappa);  
     
  
     //loop of the cg-method
-    do{
-       for(int j=0;j<XY.size();j++){ //write the XY-data to txt
-            XYfile<<XY(j)<<"\t";
-        } XYfile<<endl;
+do{
+    for(int j=0;j<XY.size();j++){ //write the XY-data to txt
+        XYfile<<XY(j)<<"\t";
+    } XYfile<<endl;
       
-      conjsteps++;
-      cout<<conjsteps<<endl;
+    conjsteps++;
+    cout<<conjsteps<<endl;
     //doSteepestDescent(XY,s0,gradE,springlist,springpairs,root,kappa);
     
-      for(int k=0;k<200;k++){
-      dEdafile<<dEda(XY+alpha[k]*s0,s0,springlist,springpairs,kappa)<<"\t";  
-    }
-    dEdafile<<endl;
+    //for(int k=0;k<99;k++){
+    //dEdafile<<dEda(XY+alpha[k]*s0,s0,springlist,springpairs,kappa)<<"\t";  
+    //}
+    //dEdafile<<endl;
       
+    doConjStep(XY,s0,gradE,springlist,springpairs,root1,kappa,conjsteps);         
+    //Rootalpha<<root1<<"\t";
       
-      doConjStep(XY,s0,gradE,springlist,springpairs,root1,kappa,conjsteps);         
-      Rootalpha<<root1<<"\t";
-      
-      
-      ESTRETCH=Energynetwork(springlist,XY);
-      EBEND=Ebend(springpairs,springlist,XY,kappa);    
-      ETOT=ESTRETCH+EBEND;    
-      lenGrad=sqrt(gradE.dot(gradE));
+    ESTRETCH=Energynetwork(springlist,XY);
+    EBEND=Ebend(springpairs,springlist,XY,kappa);    
+    ETOT=ESTRETCH+EBEND;    
+    lenGrad=sqrt(gradE.dot(gradE));
     EFile<<ESTRETCH<<"\t"<<EBEND<<"\t"<<ETOT<<"\t"<<lenGrad<<endl; //Write the Energy to a txt-file.
-    }while(conjsteps<Nit && lenGrad>tolE);
-
-    XYfile.close();
-    EFile.close();
-    dEdafile.close();
-    Rootalpha.close();
     
-FILE *fp = fopen("mikado.txt","w");
-  for(std::size_t i=0;i<mikado.size();i++){
-    fprintf(fp,"%d \t %1.8f \t %1.8f \t %1.8f \t %d \t %d\n",mikado[i].nr,mikado[i].x,mikado[i].y,
-            mikado[i].th,mikado[i].wlr,mikado[i].wud);
-  }
-  fclose(fp);
-FILE *fp2=fopen("nodes.txt","w");
- for(std::size_t i=0;i<singleNodes.size();i++){
- fprintf(fp2,"%d \t %1.8f \t %1.8f \n",singleNodes[i].number,X(i),Y(i));
- }
- fclose(fp2);
-FILE *fp3 = fopen("springs.txt","w");
-for(std::size_t i=0;i<springlist.size();i++){
-  fprintf(fp3,"%d \t %d \t %d \t %d \t %1.8f \t %1.8f \t %d\n",springlist[i].one,springlist[i].two,
-          springlist[i].wlr,springlist[i].wud,springlist[i].rlen, springlist[i].k , springlist[i].sticki);
- }
-fclose(fp3);
+}while(conjsteps<Nit && lenGrad>tolE);
 
-FILE *fp4=fopen("mikado1.txt","w");
- for(std::size_t i=0;i<mikorig.size();i++){
-  fprintf(fp4,"%1.8f \t %1.8f \t %1.8f\n",mikorig[i].x,mikorig[i].y,mikorig[i].th);
- }
- fclose(fp4);
+XYfile.close();
+EFile.close();
+dEdafile.close();
+Rootalpha.close();
+    
+
+
+// FILE *fp4=fopen("mikado1.txt","w");
+//  for(std::size_t i=0;i<mikorig.size();i++){
+//   fprintf(fp4,"%1.8f \t %1.8f \t %1.8f\n",mikorig[i].x,mikorig[i].y,mikorig[i].th);
+//  }
+//  fclose(fp4);
 
 return 0;
  }
