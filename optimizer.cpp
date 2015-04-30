b293de130566b77865f2fa8136faf4df86e1843c
Mathijs <Mathijs Vermeulen>
2015-04-30 11:20:48 +0200

    new root finding algorithms (brent , ridder) new expansion of the bending gradient.

---
+++ optimizer.cpp
@@ -0,0 +1,12 @@
+#include "random.h"
+#include <algorithm>
+#include "makemikadonetwork.h"
+#include <eigen3/Eigen/Core>
+#include <eigen3/Eigen/LU>
+#include <vector>
+#include "EnergyandGradients.h"
+#include "BendingGrad.h"
+#include<iostream>
+#include <nlopt.hpp>
+
+using namespace nlopt;
